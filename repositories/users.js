06d3d79aad12c90a0e411d8d54b11ac0db684d07
Vasily93 <markovavasily@gmail.com>
2021-07-14 16:21:10 -0700

    added create method

--- repositories/users.js
+++ repositories/users.js
@@ -13,18 +13,25 @@ class UserRepository {
             fs.writeFileSync(this.filename, '[{"name": "Vasily"}]'); //creating a file
         }
     }
-    async getAll() {
+    async getAll() { //returning all userers data
         const data = JSON.parse(await fs.promises.readFile(this.filename, {
             encoding: 'utf8'
         }));
         console.log('Content parsed ----',data);
         return data;
     }
+    async create(attrs) { //creating and adding new user to db
+        const records = await this.getAll();
+        records.push(attrs);
+        console.log(records)
+        await fs.promises.writeFile(this.filename, JSON.stringify(records));
+    }
 }
 
 const test = async () => { 
     console.log('testing!')
     const repo = new UserRepository('users.json');
+    await repo.create({name: 'Trifon'})
     await repo.getAll();
 }
 test();
